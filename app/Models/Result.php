03fb13c15b0ffd2a00385842b8af96db90ded1b9
Sourajit-Maity <you@example.com>
2021-08-16 23:03:10 +0530

    newmigration

---
+++ app/Models/Result.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Result extends Model
+{
+    use HasFactory;
+    protected $table = 'result';
+
+    protected $fillable = ['result_id','form_id','question_id','answer'];
+}
---
+++ database/migrations/2021_08_16_172438_create_questions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('question_id');
+            $table->string('question_type');
+            $table->string('question');
+            $table->string('options');
+            $table->foreignId('form_id')->nullable()->references('id')->on('forms')->onDelete('cascade');
+            $table->foreignId('company_id')->nullable()->references('id')->on('companies')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
