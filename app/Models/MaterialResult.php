f960a639f73425546bb7456c9ec91ac0018bfd3b
Sourajit-Maity <you@example.com>
2021-09-15 00:08:05 +0530

    rescon

--- app/Models/MaterialResult.php
+++ app/Models/MaterialResult.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class MaterialResult extends Model
 {
-    use HasFactory;
+    use HasFactory; 
+    protected $table = 'material_results';
+
+    protected $fillable = ['result_id','form_id','company_id','user_id',
+    'product_name','package','market','location','percentage'
+        ];
 }
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\FormController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\ResultController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,6 +31,8 @@ Auth::routes();
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/view-question-forms/{id}', [FormController::class, 'viewquestionform'])->name('view-question-forms');
 Route::get('/get_question/{id}', [FormController::class, 'getquestion'])->name('get_question');
+Route::post('/submit-answer', [ResultController::class, 'store'])->name('submit-answer');
+
 
 Route::group(['middleware' => ['auth']], function() {
     Route::resource('roles', RoleController::class);
@@ -38,4 +41,5 @@ Route::group(['middleware' => ['auth']], function() {
     Route::resource('question', QuestionController::class);
     Route::resource('form', FormController::class);
     Route::resource('companys', CompanyController::class);
+    Route::resource('results', ResultController::class);
 });
